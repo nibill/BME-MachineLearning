9c8161e19236199ac6fa0f44f0a47f32cd443b96
nibill <tet@tes.ch>
2019-11-25 19:32:49 +0100

    assignment 3 initial commit

---
+++ Assignment 3/assignment3/kmeans.py
@@ -0,0 +1,59 @@
+import numpy as np
+import time
+from matplotlib import pyplot as plt
+
+def getDistance(centroid, points):
+    return np.linalg.norm(points - centroid, axis=1)
+
+def kmeans(X, k, max_iter=100):
+    """
+    Perform k-means clusering on the data X with k number of clusters.
+
+    Args:
+        X: The data to be clustered of shape [n, num_features]
+        k: The number of cluster centers to be used
+
+    Returns:
+        centers: A matrix of the computed cluster centers of shape [k, num_features]
+        assign: A vector of cluster assignments for each example in X of shape [n]
+
+    """
+
+    centers = None
+    assign = np.zeros(len(X))
+
+    start = time.time()
+
+    #######################################################################
+    # TODO:                                                               #
+    # Perfom k-means clustering of the input data X and store the         #
+    # resulting cluster-centers as well as cluster assignments.           #
+    #                                                                     #
+    #######################################################################
+
+    # 1st step: Chose k random rows of X as initial cluster centers
+    centers = X[np.random.choice(np.arange(len(X)), k), :]
+    error = np.zeros([X.shape[0], k], dtype=np.float64)
+
+    for i in range(max_iter):
+        prev_assign = assign
+
+        # 2nd step: Update the cluster assignment
+        for i, c in enumerate(centers):
+            error[:, i] = getDistance(c, X)
+
+        # 3rd step: Check for convergence
+        assign = np.argmin(error, axis = 1)
+
+        # 4th step: Update the cluster centers based on the new assignment
+        for c in range(k):
+            centers[c] = np.mean(X[assign == c], 0)
+
+    #######################################################################
+    #                         END OF YOUR CODE                            #
+    #######################################################################
+
+    exec_time = time.time()-start
+    print('Number of iterations: {}, Execution time: {}s'.format(i+1, exec_time))
+
+    return centers, assign
---
+++ Assignment 3/assignment3/kmeans_colors.py
@@ -0,0 +1,35 @@
+from sklearn.cluster import KMeans
+import numpy as np
+from time import time
+from sklearn.utils import shuffle
+from sklearn.metrics import pairwise_distances_argmin
+
+def kmeans_colors(img, k, max_iter=100):
+    """
+    Performs k-means clusering on the pixel values of an image.
+    Used for color-quantization/compression.
+
+    Args:
+        img: The input color image of shape [h, w, 3]
+        k: The number of color clusters to be computed
+
+    Returns:
+        img_cl:  The color quantized image of shape [h, w, 3]
+
+    """
+
+    img_cl = None
+
+    #######################################################################
+    # TODO:                                                               #
+    # Perfom k-means clustering of the pixel values of the image img.     #
+    #######################################################################
+    img_reshaped = img.reshape((-1, 3))
+    kmeans = KMeans(n_clusters = 4, random_state = 0).fit(img_reshaped)
+    img_cl = kmeans.cluster_centers_[kmeans.labels_, :].reshape(img.shape).astype(int)
+
+    #######################################################################
+    #                         END OF YOUR CODE                            #
+    #######################################################################
+
+    return img_cl
